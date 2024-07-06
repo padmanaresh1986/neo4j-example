f1ebbd67a8498fc29cbd4f37fdcf2723318e7d8d
Padma Naresh <padmanaresh1986@gmail.com>
2024-07-06 22:40:39 +0530

    fixed issues

--- neo4j-app-ui/script.js
+++ neo4j-app-ui/script.js
@@ -1,26 +1,20 @@
 document.getElementById('search-button').addEventListener('click', () => {
-    const query = document.getElementById('query-input').value;
-  
-    const config = {
-      container_id: "viz",
-      server_url: "bolt://localhost:7687",
-      server_user: "neo4j",
-      server_password: "password",
+    const query = document.getElementById('query-input').value;  
+      const config = {
+      containerId: "viz",
+      neo4j: {
+          serverUrl: "bolt://localhost:7687",
+          serverUser: "neo4j",
+          serverPassword: "DurgaSahasra#2018",
+      },
       labels: {
-        "Character": {
-          "caption": "name",
-          "size": "pagerank",
-          "community": "community"
-        }
+          
       },
       relationships: {
-        "INTERACTS": {
-          "thickness": "weight",
-          "caption": false
-        }
+          
       },
-      initial_cypher: query
-    };
+      initialCypher: query
+  };
   
     const viz = new NeoVis.default(config);
     viz.render();
